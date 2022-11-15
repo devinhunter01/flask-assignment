eb366eb6f28da47bc2112da0908a9e403f66a451
devinhunter01 <112974009+devinhunter01@users.noreply.github.com>
2022-11-15 13:09:54 -0600

    Add files via upload

---
+++ app (1).py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def readDetails(filepath):
+    with open(filepath, 'r') as f:
+        return [line for line in f]
+
+@app.route('/')
+def home():
+	return render_template('base.html')
+
+@app.route('/user/<name>')
+def name(name):
+	return f'you are {name}, congrats'
+
+@app.route('/about')
+def about():
+	return render_template('about.html', about = readDetails('static/about'))
+
+
+app.run(host='0.0.0.0',port=8080, debug=True)
